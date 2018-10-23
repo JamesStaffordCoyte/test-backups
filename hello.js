087c2fe18b8cf2c3a7948583433461cf6b8dff39
James <jstaffordcoyte@gmail.com>
2018-10-23 06:58:12 -0700

    test

---
+++ hello.js
@@ -0,0 +1,15 @@
+let name = "James";
+console.log(`Hello ${name}`);
+console.log('Goodbye');
+let myArgs = process.argv.slice(2);
+console.log(myArgs.join(' '));
+switch (myArgs[0]) {
+case 'insult':
+console.log(`${myArgs[1]} smells quite badly.`);
+break;
+case 'compliment':
+console.log(`${myArgs[1]} is really cool`);
+break;
+default:
+console.log('Sorry, that is not something I know how to do.');
+}
